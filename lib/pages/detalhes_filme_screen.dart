e3c459a6aa5b2db75314cff6ec5caaab80da7e67
Caroline Matilde Lima Vitória <carol.lima.vitoria@gmail.com>
2023-07-22 16:21:32 -0300

    teste

--- lib/pages/detalhes_filme_screen.dart
+++ lib/pages/detalhes_filme_screen.dart
@@ -1,39 +1,4 @@
-// import 'package:flutter/material.dart';
-// import '../models/filme_model.dart';
 
-// class DetalhesFilmes extends StatelessWidget {
-//   final Filme filme;
-
-//   const DetalhesFilmes({super.key, required this.filme});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text(filme.nome),
-//       ),
-//       body: Padding(
-//         padding: const EdgeInsets.all(16.0),
-//         child: Column(
-//           crossAxisAlignment: CrossAxisAlignment.start,
-//           children: [
-//             Image.network(filme.imageUrl),
-//             const SizedBox(height: 16.0),
-//             const Text(
-//               'Sinopse:',
-//               style: TextStyle(
-//                 fontSize: 18,
-//                 fontWeight: FontWeight.bold,
-//               ),
-//             ),
-//             const SizedBox(height: 8.0),
-//             Text(filme.sinopse),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
 import 'package:flutter/material.dart';
 import '../models/filme_model.dart';
 
