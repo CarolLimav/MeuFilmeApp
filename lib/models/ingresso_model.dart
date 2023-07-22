f78c4f1d2cfa277c8f4c6f6662f155b45b19d6f6
VLopes <victorlopes9291@gmail.com>
2023-07-21 23:28:49 -0300

    #1 - Criação da Model Ingresso

---
+++ lib/models/ingresso_model.dart
@@ -0,0 +1,7 @@
+class Ingresso {
+  final double valor;
+  final String assento;
+  final String nomeFilme;
+  final String horario;
+  Ingresso({required this.valor, required this.assento, required this.nomeFilme, required this.horario});
+}
