84b088e1adf7555ee35ca7be2f3d1c43ac716ce6
Davi Azevedo <daviazevedos02@gmail.com>
2023-07-26 16:16:44 -0300

    adicionei texto 'Ainda não possui compras.'

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,60 +1,72 @@
 import 'package:flutter/material.dart';
 import 'package:meufilmeapp/pages/finalizar_page.dart';
 import 'package:meufilmeapp/pages/home_page.dart';
-class CartPage extends StatelessWidget {
-   
 
-  const CartPage({super.key, });
+class CartPage extends StatelessWidget {
+  const CartPage({
+    super.key,
+  });
 
   @override
-  Widget build(BuildContext context ) {
-   var listaIngresso = MyApp.listIngresso; 
+  Widget build(BuildContext context) {
+    var listaIngresso = MyApp.listIngresso;
     return Scaffold(
       appBar: AppBar(
         title: const Text('Minhas Compras'),
       ),
-      body: ListView.builder(
-        itemCount: listaIngresso.length,
-        itemBuilder: (context, index) {
-            return GestureDetector(
-              onTap: (){
-                Navigator.push(context, MaterialPageRoute(builder: (context) => FinalizarPage(ingresso: listaIngresso[index],)));
-              },
-              child: ListTile(
-                leading: SizedBox(
-                  width: 80,
-                  height: 80,
-                  child: Image.network(
-                    listaIngresso[index].imageUrlFilme!,
-                    fit: BoxFit.cover,
-                  ),
-                ),
-            
-                title:   Text('Filme: ${listaIngresso[index].nomeFilme}'), 
-                subtitle: Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Expanded(
-                      child: Text(listaIngresso[index].nomeCombo == null? '': listaIngresso[index].nomeCombo!,
-                  style: const TextStyle(fontWeight: FontWeight.bold),
-                ),
-            
-                    ),              
-                    Text(
-                      'Total : R\$ ${listaIngresso[index].valor}',
-                      style: const TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 16,
+      body: listaIngresso.isEmpty
+          ? const Center(
+              child: Text(
+                'Ainda não possui compras.',
+                style: TextStyle(fontSize: 18),
+              ),
+            )
+          : ListView.builder(
+              itemCount: listaIngresso.length,
+              itemBuilder: (context, index) {
+                return GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => FinalizarPage(
+                                  ingresso: listaIngresso[index],
+                                )));
+                  },
+                  child: ListTile(
+                    leading: SizedBox(
+                      width: 80,
+                      height: 80,
+                      child: Image.network(
+                        listaIngresso[index].imageUrlFilme!,
+                        fit: BoxFit.cover,
                       ),
                     ),
-                       
-                  ],
-                ),
-              ),
-            );
-         
-        },
-      ),
+                    title: Text('Filme: ${listaIngresso[index].nomeFilme}'),
+                    subtitle: Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Expanded(
+                          child: Text(
+                            listaIngresso[index].nomeCombo == null
+                                ? ''
+                                : listaIngresso[index].nomeCombo!,
+                            style: const TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                        Text(
+                          'Total : R\$ ${listaIngresso[index].valor}',
+                          style: const TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            ),
     );
   }
 }
