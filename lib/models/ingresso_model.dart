8026c36f406815cb2886280bcdb7d8d0a74becfd
VLopes <victorlopes9291@gmail.com>
2023-07-25 21:50:02 -0300

    [REF] atualizando a model ingresso

--- lib/models/ingresso_model.dart
+++ lib/models/ingresso_model.dart
@@ -1,7 +1,7 @@
 class Ingresso {
-  final double valor;
-  final String assento;
-  final String nomeFilme;
-  final String horario;
-  Ingresso({required this.valor, required this.assento, required this.nomeFilme, required this.horario});
+  double? valor;
+  String? assento;
+  String? nomeFilme;
+  String? horario;
+  Ingresso({this.valor, this.assento, this.nomeFilme, this.horario});
 }
