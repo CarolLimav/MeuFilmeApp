aeb90e0befadb3d9d20fcb96a2e32e211d5a92df
victor lopes <victorlopes9192@gmail.com>
2023-07-26 08:38:11 -0500

    [REF] model ingresso

--- lib/models/ingresso_model.dart
+++ lib/models/ingresso_model.dart
@@ -2,6 +2,7 @@ class Ingresso {
   double? valor;
   String? assento;
   String? nomeFilme;
+  String? nomeCombo;
   String? horario;
-  Ingresso({this.valor, this.assento, this.nomeFilme, this.horario});
+  Ingresso({this.valor, this.assento, this.nomeFilme, this.nomeCombo,this.horario});
 }
